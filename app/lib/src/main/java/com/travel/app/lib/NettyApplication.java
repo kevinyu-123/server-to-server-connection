7a572fcc147d547f69123b04d177ab69cd73680d
yuhyeonjoon <dbguswns123>
2022-04-25 19:03:35 +0900

    fix

--- app/lib/src/main/java/com/travel/app/lib/NettyApplication.java
+++ app/lib/src/main/java/com/travel/app/lib/NettyApplication.java
@@ -1,22 +1,22 @@
-package com.travel.app.lib;
-
-import com.travel.app.lib.UDPServer.NettyServer;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@SpringBootApplication
-@EnableAsync
-public class NettyApplication implements CommandLineRunner {
-    public static void main(String[] args) {
-        SpringApplication.run(NettyApplication.class,args);
-    }
-
-    @Async
-    @Override
-    public void run(String[] args){
-        new NettyServer().bind(8000);
-    }
-}
+//package com.travel.app.lib;
+//
+//import com.travel.app.lib.UDPServer.NettyServer;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.scheduling.annotation.Async;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//
+//@SpringBootApplication
+//@EnableAsync
+//public class NettyApplication implements CommandLineRunner {
+//    public static void main(String[] args) {
+//        SpringApplication.run(NettyApplication.class,args);
+//    }
+//
+//    @Async
+//    @Override
+//    public void run(String[] args){
+//        new NettyServer().bind(8000);
+//    }
+//}
--- app/login/src/main/java/com/travel/app/login/LoginApplication.java
+++ app/login/src/main/java/com/travel/app/login/LoginApplication.java
@@ -8,4 +8,5 @@ public class LoginApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class,args);
     }
+
 }
